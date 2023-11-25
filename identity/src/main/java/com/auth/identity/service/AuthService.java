bf0debbbd9b07709d119f3d574d53a418003b1f2
Senthuran100 <senthuran.manoharan@gmail.com>
2023-11-25 17:21:38 +1100

    Separate the logic from controller.

--- identity/src/main/java/com/auth/identity/service/AuthService.java
+++ identity/src/main/java/com/auth/identity/service/AuthService.java
@@ -1,4 +1,13 @@
 package com.auth.identity.service;
 
-public class AuthService {
+import com.auth.identity.payload.request.LoginRequest;
+import com.auth.identity.payload.request.SignupRequest;
+import com.auth.identity.payload.response.JwtResponse;
+import org.springframework.http.ResponseEntity;
+
+public interface AuthService {
+     JwtResponse authenticateUser(LoginRequest loginRequest);
+
+     ResponseEntity<?>  registerUser(SignupRequest signupRequest);
+
 }
---
+++ identity/src/main/java/com/auth/identity/service/impl/AuthServiceImpl.java
@@ -0,0 +1,112 @@
+package com.auth.identity.service.impl;
+
+import com.auth.identity.model.ERole;
+import com.auth.identity.model.Role;
+import com.auth.identity.model.User;
+import com.auth.identity.payload.request.LoginRequest;
+import com.auth.identity.payload.request.SignupRequest;
+import com.auth.identity.payload.response.JwtResponse;
+import com.auth.identity.payload.response.MessageResponse;
+import com.auth.identity.repository.RoleRepository;
+import com.auth.identity.repository.UserRepository;
+import com.auth.identity.security.jwt.JwtUtils;
+import com.auth.identity.security.services.UserDetailsImpl;
+import com.auth.identity.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+@Service
+public class AuthServiceImpl implements AuthService {
+
+    private AuthenticationManager authenticationManager;
+    private JwtUtils jwtUtils;
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private PasswordEncoder encoder;
+    @Override
+    public JwtResponse authenticateUser(LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        return new JwtResponse(jwt,"Bearer",
+                userDetails.getId(),
+                userDetails.getUsername(),
+                userDetails.getEmail(),
+                roles);
+    }
+
+    @Override
+    public ResponseEntity<?> registerUser(SignupRequest signUpRequest) {
+        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Username is already taken!"));
+        }
+
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Email is already in use!"));
+        }
+
+        // Create new user's account
+        User user = new User(signUpRequest.getUsername(),
+                signUpRequest.getEmail(),
+                encoder.encode(signUpRequest.getPassword()));
+
+        Set<String> strRoles = signUpRequest.getRole();
+        Set<Role> roles = new HashSet<>();
+
+        if (strRoles == null) {
+            Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+            roles.add(userRole);
+        } else {
+            strRoles.forEach(role -> {
+                switch (role) {
+                    case "admin":
+                        Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(adminRole);
+
+                        break;
+                    case "mod":
+                        Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(modRole);
+
+                        break;
+                    default:
+                        Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(userRole);
+                }
+            });
+        }
+
+        user.setRoles(roles);
+
+        userRepository.save(user);
+        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+    }
+}
