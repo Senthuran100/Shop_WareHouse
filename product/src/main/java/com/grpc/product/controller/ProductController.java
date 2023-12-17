81c3b34f51f2c6db9f074782ed4fad0c42ab8f05
Senthuran100 <senthuran.manoharan@gmail.com>
2023-12-17 12:38:48 +1100

    Fix null values in category creation.

--- product/src/main/java/com/grpc/product/controller/ProductController.java
+++ product/src/main/java/com/grpc/product/controller/ProductController.java
@@ -6,27 +6,30 @@ import com.grpc.product.payload.response.CategoryResponse;
 import com.grpc.product.payload.response.ProductResponse;
 import com.grpc.product.service.impl.CategoryServiceImpl;
 import com.grpc.product.service.impl.ProductServiceImpl;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("v1/api")
+@AllArgsConstructor
 public class ProductController {
 
     private CategoryServiceImpl categoryService;
     private ProductServiceImpl productService;
 
     @PostMapping("/category")
-    public ResponseEntity<CategoryResponse> saveCategory(CategoryRequest categoryRequest) {
+    public ResponseEntity<CategoryResponse> saveCategory(@RequestBody CategoryRequest categoryRequest) {
         ResponseEntity<CategoryResponse> categoryResponseResponseEntity = new ResponseEntity<>(categoryService.saveCategory(categoryRequest),HttpStatus.CREATED);
         return categoryResponseResponseEntity;
     }
 
     @PostMapping("/product")
-    public ResponseEntity<ProductResponse> saveProduct(ProductRequest productRequest) {
+    public ResponseEntity<ProductResponse> saveProduct(@RequestBody ProductRequest productRequest) {
        ResponseEntity<ProductResponse> productResponseResponseEntity = new ResponseEntity<>(productService.saveProduct(productRequest), HttpStatus.CREATED);
        return productResponseResponseEntity;
     }
--- product/src/main/java/com/grpc/product/payload/request/CategoryRequest.java
+++ product/src/main/java/com/grpc/product/payload/request/CategoryRequest.java
@@ -1,14 +1,14 @@
 package com.grpc.product.payload.request;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoryRequest {
     @NotBlank
     private String name;
