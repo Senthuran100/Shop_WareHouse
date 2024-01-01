83b97c9bcba5ceaed7f11ee247f87156a8e690e9
Senthuran100 <senthuran.manoharan@gmail.com>
2024-01-01 19:21:50 +1100

    Add README.md file.

--- identity/src/main/java/com/auth/identity/security/jwt/JwtUtils.java
+++ identity/src/main/java/com/auth/identity/security/jwt/JwtUtils.java
@@ -5,9 +5,11 @@ import io.jsonwebtoken.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.List;
 
 @Slf4j
 @Component
@@ -22,10 +24,13 @@ public class JwtUtils {
     public String generateJwtToken(Authentication authentication) {
 
         UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
+        List<String> roles = userPrincipal.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
 
         return Jwts.builder()
                 .setSubject(userPrincipal.getUsername())
-                .claim("roles",userPrincipal.getAuthorities().toString())
+                .claim("roles",roles)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
                 .signWith(SignatureAlgorithm.HS512, jwtSecret)
